3fe4131f56a8698c558f17467fef1355d50fb2f5
gwenparadis <gwendelynparadis2@gmail.com>
2022-11-07 19:08:41 -0500

    working current weather function retrieves location and data from openweathermap, to be used for testing between working API's

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 //declare global variables
-const searchBtn = document.getElementById("search")
-let weatherResult;
+const searchBtn = document.getElementById("search");
+const searchBar = document.getElementById("userInput");
 
 //fetch request to get weather forecast information
-function getLocation() {
+function getCurrentLocationWeather() {
     //grab API url
-    const forecastUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=boston,MA,US&appid=6f4f8d8e13827c9d81f342b6e1821c12';
+    const forecastUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + (userInput.value) + 'US&appid=6f4f8d8e13827c9d81f342b6e1821c12';
 
     fetch(forecastUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+            lat = data[0].lat;
+            lon = data[0].lon;
+
+            console.log('the latitude and longitude coordinates are: ' + lat + " ," + lon);
+
+            const currentWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&APPID=6f4f8d8e13827c9d81f342b6e1821c12';
+            return fetch(currentWeatherUrl);
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
             console.log(data);
-            const lat = data[0].lat
-            const lon = data[0].lon
-
-            //variable for the correct weather API link
-            //create fetch request for weather information from that lat lon data above
-            //fetch(weatherUrl)
-
+            console.log(data.main);
+        })
+        .catch(function (error) {
+            console.log(error);
         });
 };
 
 //Add Event Listener on the Search Button
-searchBtn.addEventListener('click', getLocation());
+searchBtn.addEventListener('click', getCurrentLocationWeather);
