7528d8185033e7fad54ec0908b67eff531c74a0a
gwenparadis <gwendelynparadis2@gmail.com>
2022-11-08 21:19:17 -0500

    added the last getEventWeather function, currently it loops over the 10 events and grabs the 5-day 3-hour weather forecast at each event location, and logs it in the console

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const searchBtn = document.getElementById("search");
 const searchBar = document.getElementById("userInput");
 
 //fetch request to get weather forecast information
-function getCurrentLocationWeather() {
-    //grab API url
+function getEvents() {
+    //url to fetch the location from the user input entered into the search bar by the user, from the openweathermap API
     const locationUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + (userInput.value) + 'US&appid=6f4f8d8e13827c9d81f342b6e1821c12';
 
+    //fetch location
     fetch(locationUrl)
         .then(function (response) {
             return response.json();
@@ -15,11 +16,10 @@ function getCurrentLocationWeather() {
         .then(function (data) {
             lat = data[0].lat;
             lon = data[0].lon;
-
             console.log('the latitude and longitude coordinates are: ' + lat + " ," + lon);
 
-            // Creates list of events using lat and lon grabbed from above
-
+            //fetch 10 events using the location fetched above
+            //url to fetch the events from the seatgeek API
             const requestEventList = 'https://api.seatgeek.com/events/?client_id=MzAxMTEzMjh8MTY2NzUxOTY1My4wNDQyMzY3&lat=' + lat + '&lon=' + lon + '&range=12mi&per_page=10'
             return fetch(requestEventList)
         })
@@ -33,15 +33,40 @@ function getCurrentLocationWeather() {
                 const eventVenue = data.events[i].venue.name;
                 const eventTime = data.events[i].datetime_local;
                 console.log(eventName, eventVenue, eventTime);
+
             }
+            getEventWeather(data);
         })
         .catch(function (error) {
             console.log(error);
-        });
+        })
 };
 
-//Add Event Listener on the Search Button
-searchBtn.addEventListener('click', getCurrentLocationWeather);
+function getEventWeather(data) {
+    //make sure the function is using the right data:
+    console.log(data);
+    //get the local date and time for each event
+    for (let i = 0; i < data.events.length; i++) {
+        console.log(data.events[i].datetime_local);
+
+        //fetch the weather for the date/time/location for each event if event is within 5 days
+        const eventLat = data.events[i].venue.location.lat;
+        const eventLon = data.events[i].venue.location.lon;
+
+        //url to fetch weather forecast from the API
+        const getWeatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + eventLat + "&lon=" + eventLon + "&units=imperial&exclude=current,minutely,hourly,alerts&appid=6f4f8d8e13827c9d81f342b6e1821c12"
+        fetch(getWeatherUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            });
+    };
+};
+
+    //Add Event Listener on the Search Button
+    searchBtn.addEventListener('click', getEvents);
 
 
 // SeatGeek API -- If time left, will use this to display local events when user loads app
